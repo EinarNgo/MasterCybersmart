523a0afc46e2c555e5db15349db73a4f9ec544eb
EinarNgo <31406902+EinarNgo@users.noreply.github.com>
2021-01-11 18:10:03 +0100

    Cognito added with loggin screen

--- App.js
+++ App.js
@@ -1,21 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
+import { withAuthenticator } from 'aws-amplify-react-native';
 
-export default function App() {
+
+Amplify.configure(config);
+
+function App() {
+  async function signOut() {
+    try {
+      await Auth.signOut();
+    } catch (error) {
+      console.log('Error signing out: ', error);
+    }
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text><img draggable="false" class="emoji" alt="💙" src="https://s.w.org/images/core/emoji/11/svg/1f499.svg"/> + <img draggable="false" class="emoji" alt="💛" src="https://s.w.org/images/core/emoji/11/svg/1f49b.svg"/> = React Native + Amplify </Text>
+      <Button title="Sign Out" color="tomato" onPress={signOut} />
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// make sure to remove the second argument
+export default withAuthenticator(App);

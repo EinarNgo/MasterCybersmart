4df699eaa2bfa2f2e57041cf2aefb637c80f4a51
EinarNgo <31406902+EinarNgo@users.noreply.github.com>
2021-01-12 00:42:56 +0100

    SignInn and SignOut added

--- App.js
+++ App.js
@@ -1,28 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Amplify from 'aws-amplify';
+import React, { useState, useEffect } from 'react';
+import { ActivityIndicator, View } from 'react-native';
+import Amplify, { Auth } from 'aws-amplify';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import config from './aws-exports';
-import { withAuthenticator } from 'aws-amplify-react-native';
-
-
+import SignIn from './screens/SignIn';
+import SignUp from './screens/SignUp';
+import ConfirmSignUp from './screens/ConfirmSignUp';
+import Home from './screens/Home';
 Amplify.configure(config);
 
-function App() {
-  async function signOut() {
-    try {
-      await Auth.signOut();
-    } catch (error) {
-      console.log('Error signing out: ', error);
-    }
-  }
+const AuthenticationStack = createStackNavigator();
+const AppStack = createStackNavigator();
+const AuthenticationNavigator = props => {
   return (
-    <View style={styles.container}>
-      <Text><img draggable="false" class="emoji" alt="💙" src="https://s.w.org/images/core/emoji/11/svg/1f499.svg"/> + <img draggable="false" class="emoji" alt="💛" src="https://s.w.org/images/core/emoji/11/svg/1f49b.svg"/> = React Native + Amplify </Text>
-      <Button title="Sign Out" color="tomato" onPress={signOut} />
-      <StatusBar style="auto" />
+    <AuthenticationStack.Navigator headerMode="none">
+      <AuthenticationStack.Screen name="SignIn">
+        {screenProps => (
+          <SignIn {...screenProps} updateAuthState={props.updateAuthState} />
+        )}
+      </AuthenticationStack.Screen>
+      <AuthenticationStack.Screen name="SignUp" component={SignUp} />
+      <AuthenticationStack.Screen
+        name="ConfirmSignUp"
+        component={ConfirmSignUp}
+      />
+    </AuthenticationStack.Navigator>
+  );
+};
+
+const AppNavigator = props => {
+  return (
+    <AppStack.Navigator>
+      <AppStack.Screen name="Home">
+        {screenProps => (
+          <Home {...screenProps} updateAuthState={props.updateAuthState} />
+        )}
+      </AppStack.Screen>
+    </AppStack.Navigator>
+  );
+};
+
+const Initializing = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <ActivityIndicator size="large" color="tomato" />
     </View>
   );
+};
+
+function App() {
+  const [isUserLoggedIn, setUserLoggedIn] = useState('initializing');
+  useEffect(() => {
+    checkAuthState();
+  }, []);
+  async function checkAuthState() {
+    try {
+      await Auth.currentAuthenticatedUser();
+      console.log('User is signed in');
+      setUserLoggedIn('loggedIn');
+    } catch (err) {
+      console.log('User is not signed in');
+      setUserLoggedIn('loggedOut');
+    }
+  }
+  function updateAuthState(isUserLoggedIn) {
+    setUserLoggedIn(isUserLoggedIn);
+  }
+  return (
+    <NavigationContainer>
+      {isUserLoggedIn === 'initializing' && <Initializing />}
+      {isUserLoggedIn === 'loggedIn' && (
+        <AppNavigator updateAuthState={updateAuthState} />
+      )}
+      {isUserLoggedIn === 'loggedOut' && (
+        <AuthenticationNavigator updateAuthState={updateAuthState} />
+      )}
+    </NavigationContainer>
+  );
 }
-// make sure to remove the second argument
-export default withAuthenticator(App);
+export default App;
